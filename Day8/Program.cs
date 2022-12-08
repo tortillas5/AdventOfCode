188cf960ce408c0e455495e16dbac1417ee7def0
tortillas5 <tortillas542@gmail.com>
2022-12-08 23:15:30 +0100

    Day8 part2

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -10,7 +10,6 @@ namespace Day8
         {
             var input = System.IO.File.ReadAllText(@"input.txt").Split('\n').Where(s => !string.IsNullOrWhiteSpace(s)).Select(s => s.ToArray()).ToArray();
             int visibleTree = input.Length * 2 + input[0].Length * 2 - 4;
-            List<int> scoreDarbre = new List<int>();
 
             for (int y = 1; y < input.Length - 1; y++)
             {
@@ -69,7 +68,86 @@ namespace Day8
                 }
             }
 
+            // Part 1
             Console.WriteLine(visibleTree);
+
+            List<int> scoreDarbre = new List<int>();
+
+            for (int y = 0; y < input.Length; y++)
+            {
+                var currentY = input[y];
+
+                for (int x = 0; x < currentY.Length; x++)
+                {
+                    // Récupère un arbre
+                    short currentX = short.Parse(currentY[x].ToString());
+                    int scoreXavant = 0;
+                    int scoreXapres = 0;
+                    int scoreYavant = 0;
+                    int scoreYapres = 0;
+
+                    // x avant
+                    for (int i = x - 1; i >= 0; i--)
+                    {
+                        if (short.Parse(currentY[i].ToString()) < currentX)
+                        {
+                            scoreXavant = x - i;
+                        }
+                        else if (short.Parse(currentY[i].ToString()) >= currentX)
+                        {
+                            scoreXavant = x - i;
+                            break;
+                        }
+                    }
+
+                    // x après
+                    for (int i = x + 1; i < input[y].Length; i++)
+                    {
+                        if (short.Parse(currentY[i].ToString()) < currentX)
+                        {
+                            scoreXapres = i - x;
+                        }
+                        else if (short.Parse(currentY[i].ToString()) >= currentX)
+                        {
+                            scoreXapres = i - x;
+                            break;
+                        }
+                    }
+
+                    // y avant
+                    for (int i = y - 1; i >= 0; i--)
+                    {
+                        if (short.Parse(input[i][x].ToString()) < currentX)
+                        {
+                            scoreYavant = y - i;
+                        }
+                        else if (short.Parse(input[i][x].ToString()) >= currentX)
+                        {
+                            scoreYavant = y - i;
+                            break;
+                        }
+                    }
+
+                    // y après
+                    for (int i = y + 1; i < input.Length; i++)
+                    {
+                        if (short.Parse(input[i][x].ToString()) < currentX)
+                        {
+                            scoreYapres = i - y;
+                        }
+                        else if (short.Parse(input[i][x].ToString()) >= currentX)
+                        {
+                            scoreYapres = i - y;
+                            break;
+                        }
+                    }
+
+                    scoreDarbre.Add(scoreXavant * scoreXapres * scoreYavant * scoreYapres);
+                }
+            }
+
+            // BigTree part 2
+            Console.WriteLine(scoreDarbre.Max());
         }
     }
 }
