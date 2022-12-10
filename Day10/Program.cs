4dc4a4a9f050295600c1d267323218044796dd98
tortillas5 <tortillas542@gmail.com>
2022-12-10 12:10:39 +0100

    Day10 P1

---
+++ Day10/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+
+namespace Day10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = System.IO.File.ReadAllText(@"input.txt").Split('\n').Where(s => !string.IsNullOrEmpty(s)).ToList();
+
+            int cycle = 1;
+            int currentIndexInput = 0;
+            int remainingCycle = 0;
+            int toAdd = 0;
+            int x = 1;
+
+            while (input.Count() > currentIndexInput)
+            {
+                if (remainingCycle == 0)
+                {
+                    var instruct = input[currentIndexInput].Split(' ');
+
+                    if (instruct[0] == "noop")
+                    {   
+                        remainingCycle++;
+                    }
+                    else if (instruct[0] == "addx")
+                    {
+                        toAdd = int.Parse(instruct[1]);
+                        remainingCycle += 2;
+                    }
+
+                    currentIndexInput++;
+                }
+
+                remainingCycle--;
+
+                if (remainingCycle == 0)
+                {
+                    // AddX
+                    x += toAdd;
+                    toAdd = 0;
+                }
+
+                cycle++;
+
+                WriteXAndSignalStrength(cycle, x);
+            }
+        }
+
+        public static int SumStrength = 0;
+
+        public static void WriteXAndSignalStrength(int cycle, int x)
+        {
+            if (new int[] { 20, 60, 100, 140, 180, 220 }.Contains(cycle))
+            {
+                SumStrength += cycle * x;
+
+                Console.WriteLine($"cycle {cycle}, x value {x}, signal strength {cycle * x}");
+                Console.WriteLine($"sum strength {SumStrength}");
+            }
+        }
+    }
+}
